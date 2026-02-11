5ca4f398727423dfd4181f6f95a06981dc19715c
YuXuanDevanLin <lin13665010834@gmail.com>
2026-02-11 12:31:00 -0500

    d

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import sample from '@/views/sample.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'sample',
+      component: sample
+    }
+  ],
 })
 
 export default router
---
+++ app/src/views/sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Cookie Clicker</h1>
+    <h2>{{ count }}</h2>
+    <img @click="increment" src="" alt="" />
+    <button>reset</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped>
+
+</style>
