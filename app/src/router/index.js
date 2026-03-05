dba934d54984231e957b15f770284b928b0bb3ec
YuXuanDevanLin <lin13665010834@gmail.com>
2026-03-05 12:31:21 -0500

    f

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import sample from '@/views/sample.vue'
+import Fishing from '@/views/fishingShore.vue'
+import Market from '@views/fishMarket.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'sample',
-      component: sample,
+      name: 'Fishing',
+      component: Fishing,
+    },
+    {
+      path: '/market',
+      name: 'Market',
+      component: Market,
     },
   ],
 })
